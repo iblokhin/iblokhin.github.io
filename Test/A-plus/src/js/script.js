65df9d453b50e5a86d969917a56df4761433f0fe
iblokhin <ihor.blokhin@gmai.com>
2017-02-13 10:26:56 +0200

    A-plus

---
+++ Test/A-plus/src/js/script.js
@@ -0,0 +1,202 @@
+$(document).ready(function() {
+
+    $('#owl-carousel').owlCarousel({
+        stagePadding: 200,
+        loop: true,
+        margin: 35,
+        navigation : false,
+        items: 1,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 28,
+                margin: 12,
+                nav : false,
+                navText: false
+            },
+            768: {
+                items: 1,
+                stagePadding: 150
+            },
+            1025: {
+                items: 1
+            }
+        },
+
+        navText : ['<span class="arrow-left"></span>','<span class="arrow-right"></span>']
+
+    });
+
+    $('#owl-carousel2').owlCarousel({
+        stagePadding: 270,
+        loop: true,
+        margin: 70,
+        navigation : false,
+        items: 1,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 28,
+                margin: 12,
+                nav : false,
+                navText: false
+            },
+            480: {
+                items: 1,
+                stagePadding: 150,
+                margin: 65,
+                nav : false,
+                navText: false
+            },
+            768: {
+                items: 1,
+                stagePadding: 240,
+                margin: 100,
+                nav : false,
+                navText: false
+            },
+            1024: {
+                items: 1,
+                stagePadding: 195,
+                margin: 95
+            },
+            1200: {
+                items: 1
+            }
+        },
+
+        navText : ['<span class="arrow-left"></span>','<span class="arrow-right"></span>']
+
+
+    });
+
+    $('#owl-carousel3').owlCarousel({
+        stagePadding: 270,
+        loop: true,
+        margin: 70,
+        navigation : false,
+        items: 1,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 28,
+                margin: 12,
+                nav : false,
+                navText: false
+            },
+            480: {
+                items: 1,
+                stagePadding: 150,
+                margin: 65,
+                nav : false,
+                navText: false
+            },
+            768: {
+                items: 1,
+                stagePadding: 240,
+                margin: 100,
+                nav : false,
+                navText: false
+            },
+            1024: {
+                items: 1,
+                stagePadding: 195,
+                margin: 95
+            },
+            1200: {
+                items: 1
+            }
+        },
+
+        navText : ['<span class="arrow-left"></span>','<span class="arrow-right"></span>']
+
+
+
+    });
+
+    $('#owl-carousel4').owlCarousel({
+        stagePadding: 270,
+        loop: true,
+        margin: 70,
+        navigation : false,
+        items: 1,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 28,
+                margin: 12,
+                nav : false,
+                navText: false
+            },
+            480: {
+                items: 1,
+                stagePadding: 150,
+                margin: 65,
+                nav : false,
+                navText: false
+            },
+            768: {
+                items: 1,
+                stagePadding: 240,
+                margin: 100,
+                nav : false,
+                navText: false
+            },
+            1024: {
+                items: 1,
+                stagePadding: 195,
+                margin: 95
+            },
+            1200: {
+                items: 1
+            }
+        },
+
+        navText : ['<span class="arrow-left"></span>','<span class="arrow-right"></span>']
+
+    });
+
+    $('#owl-carousel5').owlCarousel({
+        stagePadding: 270,
+        loop: true,
+        margin: 70,
+        navigation : false,
+        items: 1,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 28,
+                margin: 12,
+                nav : false,
+                navText: false
+            },
+            480: {
+                items: 1,
+                stagePadding: 150,
+                margin: 65,
+                nav : false,
+                navText: false
+            },
+            768: {
+                items: 1,
+                stagePadding: 240,
+                margin: 100,
+                nav : false,
+                navText: false
+            },
+            1024: {
+                items: 1,
+                stagePadding: 195,
+                margin: 95
+            },
+            1200: {
+                items: 1
+            }
+
+        },
+
+        navText : ['<span class="arrow-left"></span>','<span class="arrow-right"></span>']
+
+    });
+
+});
